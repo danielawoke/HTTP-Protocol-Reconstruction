f406ccb8a0c3b6f6dabcbdc5d3241cff4d570715
Daniel Awoke <72922216+danielawoke@users.noreply.github.com>
2026-01-05 18:26:27 -0500

    Add files via upload

---
+++ protocol/engieer.js
@@ -0,0 +1,84 @@
+// Divide into requests and responses
+
+// how do i get a request sent to a server?
+
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Guides/Overview#components_of_http-based_systems
+// HTTP flow section
+// 1 open tcp connection
+// 2 pass the http message through
+// 3 read the response by the server
+// 4 close connection
+
+// "Implmenting FTP in js depends on the enviormnet"
+
+// "the browser first needs the server's IP address. It performs a Domain Name System (DNS) lookup, 
+
+// https://www.youtube.com/watch?v=PS0UppB3-fg DNS server
+// query the local DNS server
+
+const { execSync } = require('child_process');
+const { readFile } = require('fs');
+const fs = require('fs');
+
+function dnsResolution(domain){
+    let outputBuffer = execSync('ipconfig /displaydns');
+    const DNSCachedIPsString = outputBuffer.toString();
+    const DNSCachedIPsMap = DNSCachedIPsStringToMap(DNSCachedIPsString);
+
+    if(DNSCachedIPsMap.get(domain) != null){
+        return [DNSCachedIPsMap.get(domain)];
+    }
+    
+    outputBuffer = execSync(`nslookup ${domain}`);
+    let LocalDNSQueryString = outputBuffer.toString()
+    const IPAddresses  = LocalDNSQueryStringParse(LocalDNSQueryString);
+    return IPAddresses;
+
+}
+
+function LocalDNSQueryStringParse(LocalDNSQueryString){
+
+    const ipRegex =
+    /\b(?:\d{1,3}\.){3}\d{1,3}\b|\b(?:[a-fA-F0-9]{1,4}:){2,7}[a-fA-F0-9]{0,4}\b/g;
+    const ips = LocalDNSQueryString.match(ipRegex) || [];
+    return ips;
+}
+
+function DNSCachedIPsStringToMap(DNSCachedIPsString){
+    const DNSCacheMap = new Map();
+    DNSCachedIPs = DNSCachedIPsString.split("----------------------------------------");
+    for(let i = 0; i<DNSCachedIPs.length; i++){
+        let DomainKeyString = "Record Name . . . . . : ";
+        DomainStringIndex = DNSCachedIPs[i].indexOf(DomainKeyString);
+        DomainWithTrailing = DNSCachedIPs[i].substring(DomainStringIndex+DomainKeyString.length);
+        Domain = DomainWithTrailing.substring(0,DomainWithTrailing.indexOf("\n")-1);
+
+        let IPKeyString = "A (Host) Record . . . : ";
+        IPStringIndex = DNSCachedIPs[i].indexOf(IPKeyString);
+        if(IPStringIndex!=-1){
+            IPWithTrailing = DNSCachedIPs[i].substring(IPStringIndex+IPKeyString.length);
+            IP = IPWithTrailing.substring(0, IPWithTrailing.indexOf("\n")-1);
+            DNSCacheMap.set(Domain,IP);
+        }
+
+        IPKeyString = "AAAA Record . . . . . : ";
+        IPStringIndex = DNSCachedIPs[i].indexOf(IPKeyString);
+        if(IPStringIndex!=-1){
+            IPWithTrailing = DNSCachedIPs[i].substring(IPStringIndex+IPKeyString.length);
+            IP = IPWithTrailing.substring(0, IPWithTrailing.indexOf("\n")-1);
+            DNSCacheMap.set(Domain,IP);
+            continue;
+        }
+
+        
+    }
+    
+    return DNSCacheMap;
+
+}
+
+function ThreewayHandshake(IPAdress){
+    
+}
+
+let output = execSync('sudo hping3');
