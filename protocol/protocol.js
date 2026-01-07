bbfff7a6ddf2f451275d41416b626829105fff18
Daniel Awoke <72922216+danielawoke@users.noreply.github.com>
2026-01-07 12:54:22 -0500

    Enhance HTTPProtocol with DNS and connection handling
    Refactor HTTPProtocol to support Bash password and improve DNS resolution. Update ThreeWayHandshake to handle connections more effectively.

--- protocol/protocol.js
+++ protocol/protocol.js
@@ -1,11 +1,34 @@
-const { execSync } = require('child_process');
+const { execSync, exec, spawn } = require('child_process');
 const { readFile } = require('fs');
 const fs = require('fs');
 
-function HTTPProtocol(HTTPRequestType,Domain,Paylaod){
-    IP = dnsResolution(Domain);
-    ThreeWayHandshake(IP);
+
+//This is the Primary function for creating requests. 
+//This function will provide a file for the HTTP 
+//request and response locally when ran.
+
+// Example inputs:
+// HTTPProtocol("GET", "www.google.com",null,"password")
+// HTTPProtocol("POST", "www.amazon.com",{load:2,blocks:"EMPTY"},"password")
+
+function HTTPProtocol(HTTPRequestType,Domain,Paylaod,BashPassword){
+    DomainAndEndPointArray = splitUrl(Domain);
+    let AllIPAddresses = dnsResolution(DomainAndEndPointArray[0]);
+    const IPv4Address = AllIPAddresses.filter(isValidIPv4);
+    let connection = ThreeWayHandshake(IPv4Address[0], BashPassword);
+    setTimeout(() => {
+        connection.kill();
+        console.log("kiiled");
+        execSync("npx kill-port 80");
+        let httpResponse = ["Null Response, check domain"];
+        HTTPRequest(HTTPRequestType, DomainAndEndPointArray[1], Paylaod, IPv4Address[0], BashPassword);
+    }, 1000);
     
+}
+
+function isValidIPv4(ip) {
+    const ipv4Regex = /^(?:[1-9]?\d|1\d\d|2(?:[0-4]\d|5[0-5]))(?:\.(?:[1-9]?\d|1\d\d|2(?:[0-4]\d|5[0-5]))){3}$/;
+    return ipv4Regex.test(ip);
 }
 
 function dnsResolution(domain){
@@ -49,24 +72,86 @@ function DNSCachedIPsStringToMap(DNSCachedIPsString){
             DNSCacheMap.set(Domain,IP);
         }
 
-        IPKeyString = "AAAA Record . . . . . : ";
-        IPStringIndex = DNSCachedIPs[i].indexOf(IPKeyString);
-        if(IPStringIndex!=-1){
-            IPWithTrailing = DNSCachedIPs[i].substring(IPStringIndex+IPKeyString.length);
-            IP = IPWithTrailing.substring(0, IPWithTrailing.indexOf("\n")-1);
-            DNSCacheMap.set(Domain,IP);
-            continue;
-        }
+        // Ommitted since IPv6 is incompatible with hping3
 
-        
+        // IPKeyString = "AAAA Record . . . . . : ";
+        // IPStringIndex = DNSCachedIPs[i].indexOf(IPKeyString);
+        // if(IPStringIndex!=-1){
+        //     IPWithTrailing = DNSCachedIPs[i].substring(IPStringIndex+IPKeyString.length);
+        //     IP = IPWithTrailing.substring(0, IPWithTrailing.indexOf("\n")-1);
+        //     DNSCacheMap.set(Domain,IP);
+        //     continue;
+        // }
+
     }
     
     return DNSCacheMap;
 
 }
-function SYN(){
-    let output = execSync(`sudo hping3 -S -p 80 ${IPAdress}`);
+
+function ThreeWayHandshake(IPAdress, BashPassword){
+    fileOfCommandsString = `bash\nsudo -S hping3 -S -p 80 --ack ${IPAdress}\n${BashPassword}\n`;
+    try {
+        fs.writeFileSync('commands.txt', fileOfCommandsString);
+    } catch (err) {
+        console.error(err);
+    }
+    const shellCommand = process.platform === 'win32' ? 'cmd.exe' : '/bin/sh';
+    const connection = spawn(shellCommand, [], {
+        stdio: ['pipe', process.stdout, process.stderr]
+    });
+    const commandStream = fs.createReadStream('commands.txt');
+    commandStream.pipe(connection.stdin);
+    return connection;
 }
-function ThreewayHandshake(IPAdress){
-    SYN();
+
+async function HTTPRequest(RequestType, URLEndPoint, Paylaod, IPAddress, BashPassword){
+    HTTPRequestFileString = `${RequestType} ${URLEndPoint} HTTP/1.1 \r\nHost: ${IPAddress}\r\nUser-Agent: hping3-custom-client\r\nAccept: text/html\r\nConnection: close\r\n`;
+    if(RequestType == "POST"){
+        const StringifiedPayload = JSON.stringify(Paylaod); 
+        const FormatedStringifiedPayload = JSON.stringify(StringifiedPayload, null, '\t');
+        HTTPRequestFileString += "\r\n"+FormatedStringifiedPayload;
+    }
+    try {
+        fs.writeFileSync('HTTPRequestFile.txt', HTTPRequestFileString);
+    } catch (err) {
+        console.error(err);
+    }
+    fileOfCommandsString = `socat - TCP:${IPAddress}:80 < HTTPRequestFile.txt\n`;
+    try {
+        fs.writeFileSync('commands.sh', fileOfCommandsString);
+    } catch (err) {
+        console.error(err);
+    }
+    setTimeout(() => {
+        try {
+            HTTPResponse = execSync(`bash -x commands.sh`);
+            try {
+                fs.writeFileSync('HTTPResponse.txt', HTTPResponse.toString());
+            } catch (err) {
+                console.error(err);
+            }
+            console.log('Script executed successfully.');
+        } catch (error) {
+            console.error(`Error executing script: ${error.message}`);
+            process.exit(1);
+        }
+    },2000);
 }
+
+function getFileSizeSyncInBytes(filePath) {
+  try {
+    const stats = fs.statSync(filePath);
+    return stats.size; // Size in bytes
+  } catch (err) {
+    console.error(err.message);
+    return -1; // Or handle the error as appropriate
+  }
+}
+
+function splitUrl(url) {
+  const u = new URL(url);
+  return [u.hostname, u.pathname + u.search + u.hash];
+}
+
+
